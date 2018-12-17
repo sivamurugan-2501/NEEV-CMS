da5068281100b4096b08f521d97d458bbda776d2
CG_sivadeve <sivakumar.murugan@capgemini.com>
2018-12-17 15:48:13 +0530

    git update

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,4 +1,21 @@
-import { apiConfigs } from './../environments/environment';
+//import { apiConfigs } from './../environments/environment';
 //import { environment } from 'src/environments/environment.prod';
 
-export const CONFIGS = apiConfigs;
+export const CONFIGS = {
+  "apiBaseURL" : "http://localhost:8000/cms",
+  "apiURLs" : {
+    "video" : {
+       "add" : "/video/add",
+       "list" : "/video/list"
+    },
+    "product" : {
+      "add" : "/product/add",
+      "list" : "/product/list"
+   },
+    "authenticate" : "/authenticate",
+    "configs" : {
+        "language" : "/config/languages"
+    }
+    
+  }
+};
